1ea5249634a5faa385263d408c5b902a21f5c344
Otilio Rios <jr.rios26@gmail.com>
2019-07-13 18:43:48 -0500

    refactored to use jQuery. Added a green/red flash to show whi wins each round

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
+//game variables
 var round = 1;
 var computerScore = 0;
 var playerScore = 0;
 
-window.onload = function() {
-  document.getElementById('number-field').value = 0;
+$(document).ready(() => {
+  $('#number-field').val('0');
   buttonActiveCheck();
-  document.getElementById("number-down").addEventListener("click", numberDown);
-  document.getElementById('number-up').addEventListener('click', numberUp);
-  document.getElementById('guess').addEventListener('click', takeTurn);
-}
+  $('#number-down').on('click', numberDown);
+  $('#number-up').on('click', numberUp);
+  $('#guess').on('click', takeTurn);
+});
 
 const numberDown = () => {
-  var text = parseInt(document.getElementById('number-field').value, 10);
-  document.getElementById('number-field').value = text -1;
+  var text = parseInt($('#number-field').val(), 10);
+  $('#number-field').val(text-1);
   buttonActiveCheck();
 }
 
 const numberUp = () => {
-  var text = parseInt(document.getElementById('number-field').value, 10);
-  document.getElementById('number-field').value = text +1;
+  var text = parseInt($('#number-field').val(), 10);
+  $('#number-field').val(text+1);
   buttonActiveCheck();
 }
 //checks the value of the player guess and determines if the numUp numDown buttons are active or not
 const buttonActiveCheck = () => {
-  var guessText = parseInt(document.getElementById('number-field').value, 10);
+  var guessText = parseInt($('#number-field').val(), 10);
   if (guessText === undefined || guessText === 0){
-    document.getElementById('number-down').disabled = true;
-    document.getElementById('number-down').innerHTML = '';
+    $('#number-down').disabled = true;
+    $('#number-down').html('');
   } else if (guessText === 10) {
-    document.getElementById('number-up').disabled = true;
-    document.getElementById('number-up').innerHTML = '';
+    $('#number-up').disabled = true;
+    $('#number-up').html('');
   }
   else {
-    document.getElementById('number-down').disabled = false;
-    document.getElementById('number-down').innerHTML = '-';
-    document.getElementById('number-up').disabled = false;
-    document.getElementById('number-up').innerHTML = '+';
+    $('#number-down').disabled = false;
+    $('#number-down').html('-');
+    $('#number-up').disabled = false;
+    $('#number-up').html('+');
   }
 
 }
 
 const takeTurn = () => {
-  var playerGuess = parseInt(document.getElementById('number-field').value);
-  var computerNumber = Math.floor((Math.random *10) + 1);
+  var playerGuess = parseInt($('#number-field').val());
+  var computerNumber = Math.floor((Math.random() * 10) +1 );
   round++;
 
   if (playerGuess === computerNumber) {
     playerScore ++;
-    updateText();
+    updateText(computerNumber);
+    $('#player-panel').addClass('correct');
+    setTimeout(() => {$('#player-panel').removeClass('correct')}, 1000);
+    $('#computer-panel').addClass('wrong');
+    setTimeout(() => {$('#computer-panel').removeClass('wrong')}, 1000);
   } else {
     computerScore ++;
-    updateText();
+    updateText(computerNumber);
+    $('#player-panel').addClass('wrong');
+    setTimeout(() => {$('#player-panel').removeClass('wrong')}, 1000);
+    $('#computer-panel').addClass('correct');
+    setTimeout(() => {$('#computer-panel').removeClass('correct')}, 1000);
   }
 }
 
-const updateText = () => {
-  document.getElementById('round').innerHTML = round;
-  document.getElementById('player-score').innerHTML = playerScore;
-  document.getElementById('computer-score').innerHTML = computerScore;
+const updateText = (num) => {
+  $('#round').html(round);
+  $('#player-score').html(playerScore);
+  $('#computer-score').html(computerScore);
+  $('#target-number').html(num)
 }
